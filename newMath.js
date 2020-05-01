a70321978dd13927e9d7f68f901f299f2e44ca03
= <=>
2020-05-01 22:07:56 +0700

    new gitignore

--- newMath.js
+++ newMath.js
@@ -1,6 +1,6 @@
 function remember(){
     let array = ['a','b','c']
     for(let i = 0; i < array.length; ++i){
-        return console.log(array[i])
+        console.log(array[i])
     }
 }
---
+++ settimeout.js
@@ -0,0 +1,1 @@
+setTimeout(function(){alert("time out")},30000)
